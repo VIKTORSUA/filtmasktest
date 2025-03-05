6ba52bfb05a378bba450f700f4eee6894d87a0ad
Viktor <viktorsua@gmail.com>
2025-03-05 16:11:43 -0500

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/facefilter/",
+  base: mode === "development" ? "/" : "/filtmasktest/",
   plugins: [react()],
 }));
